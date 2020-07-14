7bdb13bfb84979259a8f4501b6f358d12d88a9eb
Anil Nepal <anilnepal14949@gmail.com>
2020-07-14 12:30:59 +1000

    Database seeder updated to seed all tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,17 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-        // $this->call(CategorySeeder::class);
-        // $this->call(CategoryTranslationSeeder::class);
-        // $this->call(ProvinceSeeder::class);
-        // $this->call(ProvinceTranslationSeeder::class);
-        // $this->call(SubscriberSeeder::class);
-        // $this->call(CategorySubscriberSeeder::class);
-        // $this->call(AllResourceSeeder::class);
-        // $this->call(VideoResourceSeeder::class);
-        // $this->call(ResourceTranslationSeeder::class);
-        // $this->call(NewsSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(LanguageSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(CategoryTranslationSeeder::class);
+        $this->call(ProvinceSeeder::class);
+        $this->call(ProvinceTranslationSeeder::class);
+        $this->call(SubscriberSeeder::class);
+        $this->call(CategorySubscriberSeeder::class);
+        $this->call(AllResourceSeeder::class);
+        $this->call(VideoResourceSeeder::class);
+        $this->call(ResourceTranslationSeeder::class);
+        $this->call(NewsSeeder::class);
         $this->call(NewsTranslationSeeder::class);
+        $this->call(SocialsSeeder::class);
     }
 }
