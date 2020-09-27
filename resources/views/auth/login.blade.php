3f6814b411c84311f59d1c5771654a8b17d89ce0
Sujana Kunwar <sujana@techtatva.com>
2020-09-27 23:56:00 +1000

    login blade updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,10 +32,6 @@
 
 <body class="hold-transition login-page">
     <!-- Login Content -->
-    <div class="container-login">
-        <div class="row justify-content-center">
-
-            <div class="col-xl-3 col-sm-6 col-lg-4 col-md-6">
     <div class="login-box">
         <div class="card">
             <div class="card-body login-card-body">
@@ -70,45 +66,6 @@
                                     <div class="input-group-text">
                                         <span class="fas fa-lock"></span>
                                     </div>
-                                    <form method="POST" action="{{ route('login') }}">
-                                        @csrf
-                                        <div class="input-group mb-3">
-                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', session('remember_email', '')) }}" placeholder="Email.." required autocomplete="email" autofocus>
-                                            <div class="input-group-append">
-                                                <div class="input-group-text">
-                                                    <span class="fas fa-envelope"></span>
-                                                </div>
-                                            </div>
-                                            @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                            @enderror
-                                        </div>
-                                        <div class="input-group mb-3">
-                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password', session('remember_password', '')) }}" required autocomplete="current-password" placeholder="Password..">
-                                            <div class="input-group-append">
-                                                <div class="input-group-text">
-                                                    <span class="fas fa-lock"></span>
-                                                </div>
-                                            </div>
-                                            @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                            @enderror
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control" style="line-height: 1.5rem;">
-                                                <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} value="{{ old('remember', session('remember_me', '')) }}">
-                                                <label class="custom-control-label" for="remember">Remember
-                                                    Me</label>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <button type="submit" class="btn btn-primary btn-block">
-                                                {{ __('Login') }}
-                                            </button>
                                 </div>
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -148,6 +105,6 @@
             </div>
         </div>
     </div>
-
+                    
 </body>
 </html>
